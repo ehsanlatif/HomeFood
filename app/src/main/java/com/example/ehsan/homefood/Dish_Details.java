8eb5e90e490518159df35855c3193fd257a48227
ehsan <ehsanlatif000@gmail.com>
2018-04-19 08:23:17 +0500

    commited by Ehsan

---
+++ app/src/main/java/com/example/ehsan/homefood/Dish_Details.java
@@ -0,0 +1,13 @@
+package com.example.ehsan.homefood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dish_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish__details);
+    }
+}
